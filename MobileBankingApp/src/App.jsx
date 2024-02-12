495485f371605e252ccbcc28a18283b6e0016942
RusselDayao <russeldayao@gmail.com>
2024-02-12 17:47:59 +0800

    initial

---
+++ MobileBankingApp/src/App.jsx
@@ -0,0 +1,22 @@
+import styles from "./style"
+
+const App = () => {
+  return (
+    <div className='bg-primary w-full overflow-hidden'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          NavBar
+        </div>
+      </div>
+
+        <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            
+          </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default App
